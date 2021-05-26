edd9028ceebaff0d8ce85c8fcdb6cdff867c63f2
Marvin Countryman <me@maar.vin>
2021-05-25 22:14:52 -0400

    refactor(parse): add next_expr

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -7,3 +7,28 @@ pub use ast::*;
 pub use error::*;
 pub use tokens::*;
 pub use types::*;
+
+pub struct Parser<'buf> {
+  tokens: Tokenizer<'buf>,
+  peeked: Option<Option<Token<'buf>>>,
+}
+
+impl<'buf> Parser<'buf> {
+  pub fn new(buf: &'buf str) -> Self {
+    Self {
+      tokens: Tokenizer::new(buf),
+      peeked: None,
+    }
+  }
+
+  pub fn parse(&mut self) -> ParseResult<'buf, Block<'buf>> {
+    todo!()
+  }
+
+  fn next_expr(&mut self) -> ParseResult<'buf, Option<Expression<'buf>>> {
+    Ok(match self.tokens.next().transpose()? {
+      Some(token) => {}
+      None => {}
+    })
+  }
+}
