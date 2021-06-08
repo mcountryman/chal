b36f23777e1ea159bed5f3d31e6a377281747cf5
Marvin Countryman <me@maar.vin>
2021-06-08 09:28:35 -0400

    refactor(util): use u128 as backing type for uuid

--- src/util/uuid.rs
+++ src/util/uuid.rs
@@ -5,18 +5,19 @@ use std::{
 
 static COUNTER: AtomicU16 = AtomicU16::new(0);
 
-#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Hash)]
-pub struct Uuid {
-  counter: u16,
-  timestamp: Instant,
+thread_local! {
+  static EPOCH: Instant = Instant::now();
 }
+
+#[derive(Debug, Clone, Copy, Eq, Ord, PartialEq, PartialOrd, Hash)]
+pub struct Uuid(u128);
 
 impl Uuid {
   pub fn new() -> Self {
-    Self {
-      counter: COUNTER.fetch_add(1, Ordering::SeqCst),
-      timestamp: Instant::now(),
-    }
+    let count = COUNTER.fetch_add(1, Ordering::SeqCst);
+    let duration = EPOCH.with(|epoch| epoch.elapsed());
+
+    Self(duration.as_nanos().wrapping_add(count as _))
   }
 }
 
